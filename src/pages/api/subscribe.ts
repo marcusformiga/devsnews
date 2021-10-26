dc3d1c0fd911fb4913f66fcb2aa3dacd10bafbb1
marcusformiga <kiko.antares@gmail.com>
2021-10-26 12:07:58 -0300

    change

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,28 +1,27 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { stripe } from "../../services/stripe";
-import {getSession} from "next-auth/client"
+import { getSession } from "next-auth/client";
 
-export default async (req: NextApiRequest, response: NextApiResponse) => {
-    const session = await getSession({ req })
+export default async (request: NextApiRequest, response: NextApiResponse) => {
+  if (request.method === "POST") {
+    const session = await getSession({ req: request });
     const stripeCustomer = await stripe.customers.create({
-        email: session.user.email
-    })
-    if (req.method === "POST") {
-        const stripeCheckOutSession = await stripe.checkout.sessions.create({
-            customer: stripeCustomer.id,
-            payment_method_types: ["card"],
-            billing_address_collection: "required",
-            line_items: [
-                {price: "price_1JnpYiAHtfowqU82iNqujlup", quantity: 1}
-            ],
-            mode: "subscription",
-            allow_promotion_codes: true,
-            success_url: process.env.SUCCESS_URL,
-            cancel_url: process.env.CANCEL_URL
-        })
-        return response.status(201).json({sessionId: stripeCheckOutSession.id})
-    } else {
-        response.setHeader("Allow", "POST")
-        response.status(405).end("Method not allowed")
-    }
-}
+      email: session.user.email,
+    });
+
+    const stripeCheckOutSession = await stripe.checkout.sessions.create({
+      customer: stripeCustomer.id,
+      payment_method_types: ["card"],
+      billing_address_collection: "required",
+      line_items: [{ price: "price_1JnpYiAHtfowqU82iNqujlup", quantity: 1 }],
+      mode: "subscription",
+      allow_promotion_codes: true,
+      success_url: process.env.SUCCESS_URL,
+      cancel_url: process.env.CANCEL_URL,
+    });
+    return response.status(201).json({ sessionId: stripeCheckOutSession.id });
+  } else {
+    response.setHeader("Allow", "POST");
+    response.status(405).end("Method not allowed");
+  }
+};
