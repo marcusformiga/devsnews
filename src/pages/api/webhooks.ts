df9c2137df4d4f73da30fef7a3074d01eaa1d3d7
marcusformiga <kiko.antares@gmail.com>
2021-10-29 15:21:49 -0300

    config webhooks

---
+++ src/pages/api/webhooks.ts
@@ -0,0 +1,48 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { Readable } from "stream";
+import Stripe from "stripe";
+import { stripe } from "../../services/stripe";
+
+async function buffer(readdable: Readable) {
+    const chunks = []
+    for await (const chunk of readdable) {
+        chunks.push(
+            typeof chunk === "string" ? Buffer.from(chunk): chunk
+        )
+    }
+    return Buffer.concat(chunks)
+}
+export const config = {
+    api: {
+        bodyParser: false
+    }
+}
+const relevantEvents = new Set([
+    "checkout.session.completed"
+])
+export default async function handleWebhook (req: NextApiRequest, res: NextApiResponse)  {
+    if (req.method === "POST") {
+        const buff = await buffer(req)
+        const secret = req.headers["stripe-signature"]
+        let event: Stripe.Event
+        try {
+            event = stripe.webhooks.constructEvent(
+              buff,
+              secret,
+              process.env.STRIPE_WEBHOOK_SECRET
+            );
+        } catch (err) {
+            return res.status(400).send(`Webhook error ${err.message}`)
+        }
+        const {type} = event;
+        if (relevantEvents.has(type)) {
+            console.log("Evento recebido", event)
+        }
+        res.json({received: true})
+
+    } else {
+        res.setHeader("Allow", "POST")
+        res.status(405).end("Method not allowed")
+    }
+}
+
